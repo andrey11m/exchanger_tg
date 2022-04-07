10f795dff962967318149cd775d7606af18581f5
Andrei Shkurko <andrey.shkurko@mbicycle.com>
2022-04-07 14:58:03 +0300

    changed ping server

--- src/main/java/com/java/exchanger_tg/heroku/PingMe.java
+++ src/main/java/com/java/exchanger_tg/heroku/PingMe.java
@@ -2,21 +2,27 @@ package com.java.exchanger_tg.heroku;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 @Slf4j
 public class PingMe {
 
-    @Scheduled(fixedRateString = "PT20M")
+    @Scheduled(fixedRateString = "PT5M")
     @SneakyThrows
     public void ping() {
-        Document document = Jsoup.connect("https://www.google.com/")
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
-                .get();
-        document.select("#SIvCob");
+        try {
+            URL url = new URL("https://exchanger-rates.herokuapp.com/");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Server is working...  Ping {} status: {}", url.getHost(), connection.getResponseCode());
+            connection.disconnect();
+        } catch (Exception e) {
+            log.error("Error from pingMe {}", e.getMessage());
+        }
     }
 }
