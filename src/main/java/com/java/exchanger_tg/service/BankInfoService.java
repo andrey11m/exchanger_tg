a31c872a2675b0f52640f74210ba93dd12e2a705
Andrei Shkurko <andrey.shkurko@mbicycle.com>
2022-04-09 13:50:36 +0300

    added hash for bank info

---
+++ src/main/java/com/java/exchanger_tg/service/BankInfoService.java
@@ -0,0 +1,7 @@
+package com.java.exchanger_tg.service;
+
+import java.util.HashMap;
+
+public interface BankInfoService {
+    HashMap<String, String> getBankInfo();
+}
--- src/main/java/com/java/exchanger_tg/service/impl/TelegramBotServiceImpl.java
+++ src/main/java/com/java/exchanger_tg/service/impl/TelegramBotServiceImpl.java
@@ -1,6 +1,6 @@
 package com.java.exchanger_tg.service.impl;
 
-import com.java.exchanger_tg.service.HTMLParser;
+import com.java.exchanger_tg.service.BankInfoService;
 import com.java.exchanger_tg.service.TelegramBotService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
@@ -10,14 +10,12 @@ import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-public class TelegramTelegramBotServiceImpl implements TelegramBotService {
+public class TelegramBotServiceImpl implements TelegramBotService {
 
-    private final HTMLParser htmlParser;
     private final TelegramBot bot;
+    private final BankInfoService infoService;
 
     public void serve() {
         bot.setUpdatesListener(updates -> {
@@ -30,23 +28,10 @@ public class TelegramTelegramBotServiceImpl implements TelegramBotService {
         Message message = update.message();
         long chatId = message.chat().id();
         if (message.text().equals("/mtbank")) {
-            List<String> infoList = htmlParser.parse();
-            String responseMessage = createMessage(infoList);
-            bot.execute(new SendMessage(chatId, responseMessage));
+            bot.execute(new SendMessage(chatId, infoService.getBankInfo().get(MTBankParser.class.getSimpleName())));
         } else {
             bot.execute(new SendMessage(chatId, "Unknown command"));
         }
 
-    }
-
-    private String createMessage(List<String> infoList) {
-        StringBuilder response = new StringBuilder();
-        infoList.stream().map(StringBuilder::new)
-                .forEach(line -> {
-                    int index = line.indexOf("покупка");
-                    line.insert(index, ":\n");
-                    response.append(line).append("\n");
-                });
-        return String.valueOf(response);
     }
 }
